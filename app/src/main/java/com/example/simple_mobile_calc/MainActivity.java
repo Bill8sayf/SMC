f9b52194010eb8a98e892abe5de8683d5d988606
Bill8sayf <91134441+Bill8sayf@users.noreply.github.com>
2025-10-30 20:24:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simple_mobile_calc/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.simple_mobile_calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.text_field);
+
+        text = "text";
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
